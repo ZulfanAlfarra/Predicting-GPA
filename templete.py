3c2b244b958421a485dd29431a185b2bc060e85f
ZulfanAlfarra <zulfansyahidanalfarra.91@gmail.com>
2024-11-18 23:40:27 +0700

    Setup project predicting gpa

---
+++ templete.py
@@ -0,0 +1,37 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s')
+
+list_of_files = [
+    f"src/__init__.py",
+    f"src/components/__init__py",
+    f"src/pipeline/__init__py",
+    f"src/exception.py",
+    f"src/logger.py",
+    f"src/utils.py",
+    f"notebook/trial.ipynb",
+    f"templates/index.html",
+    "app.py",
+    "setup.py",
+    "requirements.txt"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file: {filename}")
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath,'w') as f:
+            logging.info(f"Creating empty file: {filepath}")
+            pass
+    
+    else:
+        logging.info(f"{filepath} is already exist")
+            
+    
