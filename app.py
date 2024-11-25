4b3164546cb90d10128b487d7b98d2acec1fb247
ZulfanAlfarra <zulfansyahidanalfarra.91@gmail.com>
2024-11-25 21:46:55 +0700

    create web app

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
 from flask import Flask, render_template, request
+from src.components.data_processor import CustomData
+from src.step.model_predicting_step import predict
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+@app.route("/", methods= ['GET', 'POST'])
+def predict_data():
+    if request.method =='POST':
+        data = CustomData(
+            study_hours_per_day=request.form.get('study_hours'),
+            extracurricular_hours_per_day=request.form.get('extracurricular_hours'),
+            sleep_hours_per_day=request.form.get('sleep_hours'),
+            social_hours_per_day=request.form.get('social_hours'),
+            physical_activity_hours_per_day=request.form.get('physical_hours'),
+            stress_level=request.form.get('stress')
+        )
+
+        df = data.get_df()
+
+        pred = predict(df)[0]
+
+        return render_template("index.html", prediction=pred)
+    
+    return render_template("index.html", prediction=None)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
